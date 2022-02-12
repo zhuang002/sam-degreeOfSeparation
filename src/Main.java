5987e81167935f3d91ad7bbb6da62b51c6d0072a
zhuang002 <zhuang001@gmail.com>
2022-02-12 15:34:49 -0500

    sample

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	
@@ -33,12 +35,13 @@ public class Main {
 				break;
 			case 'f':
 				x = sc.nextInt();
-				getNoOfFriends(nodes,x);
+				System.out.println(getNoOfFriendsOfFriends(nodes,x));
 				break;
 			case 's':
 				x = sc.nextInt();
 				y = sc.nextInt();
-				getDistance(x,y);
+				int distance = getDistance(nodes, x,y);
+				System.out.println(distance==-1?"not connected":distance );
 				break;
 			case 'q':
 				return;
@@ -48,9 +51,40 @@ public class Main {
 		}
 	}
 
-	private static void getDistance(int x, int y) {
+	private static int getNoOfFriendsOfFriends(Node[] nodes, int x) {
 		// TODO Auto-generated method stub
+		int count = 0;
+		for (Node neighbour: nodes[x].neighbours) {
+			for (Node level2Neighbour: neighbour.neighbours) {
+				if (level2Neighbour != nodes[x] && !nodes[x].neighbours.contains(level2Neighbour)) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	private static int getDistance(Node[] nodes, int x, int y) {
+		// TODO Auto-generated method stub
+		int distance = 0;
+		Set<Node> visited = new HashSet<>();
+		Set<Node> current = new HashSet<>();
+		current.add(nodes[x]);
+		Set<Node> next = new HashSet<>();
 		
+		while (!current.isEmpty()) {
+			for (Node node: current) {
+				if (node == nodes[y])
+					return distance;
+				visited.add(node);
+				next.addAll(node.neighbours);
+			}
+			next.removeAll(visited);
+			distance++;
+			current = next;
+			next = new HashSet<>();
+		}
+		return -1;
 	}
 
 	private static void getNoOfFriends(Node[] nodes, int x) {
